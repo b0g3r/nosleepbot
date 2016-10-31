695b9437fca344bc8d62dd9a8c7b962df36c5c53
b0g3r <kotvberloge@gmail.com>
2016-10-31 03:02:35 +0300

    init database moved to init.py

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn main:app --log-file -
-init: python model.py
+init: python init.py
--- model.py
+++ model.py
@@ -87,9 +87,3 @@ class User(Model):
 
     class Meta:
         database = db_proxy
-
-
-if __name__ == '__main__':
-    db_proxy.connect()
-    db_proxy.create_tables([User], safe=True)
-    db_proxy.close()
